edd7a404b5f2aa67566050866e0d4d8072e56640
Ethan Obott <ethanobott6002@gmail.com>
2026-08-26 13:38:03 +0000

    initial-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
 
-// react week 2 exercise part 1
-
 function App() {
-
   return (
-    <>
-          <div>
-              <h1>Codetrain is awesome, react works!!!!</h1>
-          </div>
-    </>
+    <main className="page">
+      <section className="content">
+        <img
+          className="hero-image"
+          src="https://images.unsplash.com/photo-1513151233558-d860c5398176?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2VsZWJyYXRpb258ZW58MHx8MHx8fDA%3D"
+          alt="Colorful celebration decorations"
+        />
+        <h1>I just learned React!</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed voluptate magni eius harum veritatis nulla dolores repellat blanditiis. Praesentium saepe provident rerum quidem beatae temporibus aut optio, eveniet rem culpa?
+        </p>
+        <h2>Learning by making</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, similique sed! Dolorum adipisci voluptates totam, dicta maxime odit id unde.
+        </p>
+      </section>
+    </main>
   )
 }
 
